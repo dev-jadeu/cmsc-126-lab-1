1f50be5c9cd0476c2db10d119422705ea700c0b1
Randall Alquicer <randall.bposeats@gmail.com>
2025-03-14 16:06:27 +0800

    adds socket parameter to physical layer classes

--- layers/physical_layer.py
+++ layers/physical_layer.py
@@ -5,11 +5,13 @@ import socket
 class PhysicalLayer:
     """The layer that simulates the usage of Python sockets and bit-level operations"""
 
-    def send(self, socket: socket.socket, data: dict) -> None:
+    def __init__(self, sock: socket.socket):
+        self.sock = sock
+
+    def send(self, data: dict) -> None:
         """Sends serialized data over a socket with a 4-byte length prefix
 
         Args:
-            socket (socket.socket): The socket to read from
             data (dict): The structured message to transmit
         """
         # Indicator that this is the layer that is sending the data
@@ -22,13 +24,10 @@ class PhysicalLayer:
         length = struct.pack('!I', len(serialized_data))
 
         # Send the length and the serialized data
-        socket.sendall(length + serialized_data)
+        self.sock.sendall(length + serialized_data)
 
-    def receive(self, socket: socket.socket) -> dict:
+    def receive(self) -> dict:
         """Receives serialized data from a socket, deserializes it, and returns it as a dictionary
-
-        Args:
-            socket (socket.socket): The socket to read from
 
         Returns:
             dict: The deserialized message structure
@@ -37,7 +36,7 @@ class PhysicalLayer:
         print("\033[96m[PHYSICAL LAYER] Receiving from socket\033[0m")
 
         # Read the first 4 bytes to get the length of the serialized data
-        raw_length = self.receive_bytes(socket, 4)
+        raw_length = self.receive_bytes(4)
 
         # If nothing is received, raise a connection close message
         if not raw_length:
@@ -47,16 +46,15 @@ class PhysicalLayer:
         length = struct.unpack('!I', raw_length)[0]
 
         # Calls the receive_bytes helper function to get the exact number of the actual data
-        serialized_data = self.receive_bytes(socket, length)
+        serialized_data = self.receive_bytes(length)
 
         # Deserialize the received bytes back into a Python object
         return pickle.loads(serialized_data)
 
-    def receive_bytes(self, socket: socket.socket, n: int) -> bytes:
+    def receive_bytes(self, n: int) -> bytes:
         """Helper method to receive exactly n bytes from the socket
 
         Args:
-            socket (socket.socket): The socket to read from
             n (int): The number of bytes to read
 
         Returns:
@@ -68,7 +66,7 @@ class PhysicalLayer:
         # Loop until the length of the data is equal to n
         while len(data) < n:
             # Receives the remaining bytes needed
-            packet = socket.recv(n - len(data))
+            packet = self.sock.recv(n - len(data))
 
             # Raise an error if the connection is closed before all data is received
             if not packet:
